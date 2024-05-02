4f1bd1d15bdad6c13a6c4351b478f43a94e696a1
AlexB19689 <AlexBugaichuk@gmail.com>
2024-05-02 19:07:45 +0500

    Add files via upload

---
+++ change-text-color.js
@@ -0,0 +1,7 @@
+/*document.querySelector('.dots-p').textContent.split('\n')
+document.querySelector('.dots-p').textContent = ''
+document.querySelector('.dots-p').appendChild(document.createElement('div'))*/
+
+alert('Гойда');
+
+
